c3f4dacbffdd99adc3f60502ae2a40a45da1be59
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-02 12:10:53 -0300

    retornando os detalhes das series

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,6 +26,15 @@ public class SerieService {
 
     public List<SerieDTO> obterLancamentos() {
         return converteDados(repository.findTop5ByOrderByEpisodioDataLancamentoDesc());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+        if (serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 
     private List<SerieDTO> converteDados(List<Serie> series){
