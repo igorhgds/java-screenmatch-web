125408a326c7617481ca158d2036097bbde99627
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-02 11:56:15 -0300

    lançamentos

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,23 +4,30 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService service;
 
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obterSeries() {
         return service.obterTodasAsSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SerieDTO> obterTop5Series() {
         return service.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterLancamentos() {
+        return service.obterLancamentos();
     }
 }
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,15 +16,21 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repository.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());    }
+        return converteDados(repository.findAll());
+   }
 
     public List<SerieDTO> obterTop5Series() {
-        return repository.findTop5ByOrderByAvaliacaoDesc()
-                .stream()
+        return converteDados(repository.findTop5ByOrderByAvaliacaoDesc());
+    }
+
+    public List<SerieDTO> obterLancamentos() {
+        return converteDados(repository.findTop5ByOrderByEpisodioDataLancamentoDesc());
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series){
+                return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
     }
+
 }
