ab50059eb8d0aec9081f57bd1de7c24dd94273a4
igorhgds <igorhenrique.gomes@hotmail.com>
2025-04-02 11:39:31 -0300

    top 5 series

---
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository repository;
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return repository.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return repository.findTop5ByOrderByAvaliacaoDesc()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+}
